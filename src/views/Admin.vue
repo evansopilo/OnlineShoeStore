88545e796bad8c799e1c41a7fd7f4abdbdb81882
evans <opiloevans96@gmail.com>
2022-03-30 00:44:13 +0300

    update admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,17 @@
 <template>
-    <h2 class="text-center mt-2">Dashboard</h2>
-    <div class="container d-flex text-center">
-        <router-link class="nav-link" aria-current="page" to="/admin/products">Products</router-link>
-        <router-link class="nav-link" aria-current="page" to="/admin/users">Users</router-link>
-    </div>
+    <!-- Admin navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="navbar-nav ms-auto">
+                <router-link class="nav-link m-2" aria-current="page" to="/">Home</router-link>
+                <router-link class="nav-link m-2" aria-current="page" to="/admin/products">Products</router-link>
+                <router-link class="nav-link m-2" aria-current="page" to="/admin/users">Users</router-link>
+            </div>
+        </div>
+    </nav>
+    <!-- end of admin navbar-->
+
+    <!-- router view to display admin windows -->
     <router-view/>
+    <!-- end of router view to display admin windows -->
 </template>
