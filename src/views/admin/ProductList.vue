3eaca7ad4a91e2bd0885bd6345ca0d8e8afdd495
evans <opiloevans96@gmail.com>
2022-04-07 08:04:28 +0300

    update viewsx

---
+++ src/views/admin/ProductList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <h4 class="m-2">Products Data</h4>
+    </div>
+    <div class="container">
+        <button class="btn btn-primary m-2">Add Product</button>
+    </div>
+    <div class="container jumbotron mt-2 bg-light">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Desc</th>
+                <th scope="col">Image</th>
+                <th scope="col">Price</th>
+                <th scope="col">Discount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <ProductCard v-for="product in products" v-bind:key="product.id" v-bind:product="product" />
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import ProductCard from "../../../components/admin/ProductCard.vue"
+export default {
+    components:{
+        ProductCard,
+    },
+    data(){
+        return {
+        products:[
+            {
+                "id":2,
+                "title":"this is title two",
+                "desc":"this is item two description",
+                "image":"https://www.image.com/image/2",
+                "price":114,
+                "discount":8
+            },
+            {
+                "id":2,
+                "title":"this is title two",
+                "desc":"this is item two description",
+                "image":"https://www.image.com/image/2",
+                "price":114,
+                "discount":8
+            },
+            {
+                "id":1,
+                "title":"this is title one",
+                "desc":"this is item one description",
+                "image":"https://www.image.com/image/1",
+                "price":154,
+                "discount":12
+            },
+        ]
+        }
+    }
+}
+</script>
---
+++ src/views/admin/UsersList.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container">
+        <h4 class="m-2">Users Data</h4>
+    </div>
+    <div class="container">
+        <button class="btn btn-primary m-2">Add User</button>
+    </div>
+</template>
