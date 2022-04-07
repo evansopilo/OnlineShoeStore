cfe705ae766a8fa73708f3939790762af1fc8f73
evans <opiloevans96@gmail.com>
2022-04-07 08:03:26 +0300

    update hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="hero" style="height:90vh">
+    <div class="hero" style="height:92vh">
         <div class="container h-100">
           <div class="row h-100 justify-content-center align-items-center">
             <div class="col-md-5">
-              <div class="hero-content">
+              <div class="hero-content text-white">
                 <h1 class="hero-title">
                   Shoe Shop
                 </h1>
-                <p>
+                <h5>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos numquam tempora, iure delectus totam minus quam aperiam ratione dolores magni voluptates ut necessitatibus odio ipsum fuga, voluptas ab praesentium nihil?
-                </p>
+                </h5>
                 <div class="hero-btn mt-5">
-                    <button class="btn btn-primary m-2">Explore</button>
+                    <button class="btn btn-dark m-2">Explore</button>
                     <button class="btn btn-outline-secondary m-2">Products</button>
                 </div>
               </div>
@@ -41,5 +41,11 @@ export default {
     width: 100%;
     height: 500px;
     text-align: left;
+  }
+  .hero{
+    background-image: url("https://wallpapercave.com/wp/ZqJ3XLB.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
   }
 </style>
