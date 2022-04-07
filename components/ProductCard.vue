88b81be534c80235aa8e1e7b7838812e805ba106
evans <opiloevans96@gmail.com>
2022-04-07 08:03:51 +0300

    update product card

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,32 +1,33 @@
 <template>
     <div class="col-md-8 col-lg-6 col-xl-4 mt-3">
-          <div class="card text-black">
-            <i class="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/3.webp"
-              class="card-img-top"
-              alt="Apple Computer"
-            />
-            <div class="card-body">
-              <div class="text-center">
-                <h5 class="card-title">Believing is seeing</h5>
-                <p class="text-muted mb-4">Apple pro display XDR</p>
-              </div>
-              <div>
-                <div class="d-flex justify-content-between">
-                  <span>Pro Display XDR</span><span>$5,999</span>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <span>Pro stand</span><span>$999</span>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <span>Vesa Mount Adapter</span><span>$199</span>
-                </div>
-              </div>
-              <div class="d-flex justify-content-between total font-weight-bold mt-4">
-                <span>Total</span><span>$7,197.00</span>
-              </div>
-            </div>
+      <div style="margin: 3rem auto; width: 20rem">
+        <div class="card">
+          <img
+            src="https://www.kindacode.com/wp-content/uploads/2021/08/phone.jpg"
+            class="card-img-top"
+            alt="Phone"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Super Phone</h5>
+            <h6 class="card-subtitle mb-2 text-muted">199$</h6>
+            <p class="card-text">
+              This is an awesome smartphone that can help you do a lot of thing in
+              this modern era. 
+            </p>
+            <button class="btn btn-dark" v-on:click="addToCart">Add to Cart</button>
           </div>
         </div>
+      </div>
+    </div>
 </template>
+
+<script>
+export default {
+ 
+  methods:{
+    addToCart(){
+      console.log('Add To Cart')
+    }
+  }
+}
+</script>
